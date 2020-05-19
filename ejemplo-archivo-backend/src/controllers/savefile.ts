7469474087f099fe7f3b2a1379120108cb49b5a0
Gabriel Solórzano <g.solorzano@estudiantec.cr>
2020-05-19 09:51:39 -0600

    Se arreglan pequeños errores

--- ejemplo-archivo-backend/src/controllers/savefile.ts
+++ ejemplo-archivo-backend/src/controllers/savefile.ts
@@ -4,21 +4,17 @@ import path from 'path'
 let filename = "";
 let ext = ""
 
-let hashCode = function (s:any) {
-    return s.split("").reduce(function (a:any, b:any) { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0);
-}
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         let url = path.join('public/' + req.body.tabla)
         cb(null, url)
     },
     filename: function (req, file, cb) {
-        //cb(null, hashCode + '-' + file.originalname)
+        let hashCode = Math.random().toString(36).replace(/[^a-z]+/g, '');
+        cb(null, hashCode + '-' + file.originalname)
         // console.log(req.body.tabla);
         console.log(file.mimetype);
         ext = file.mimetype;
-        let hashCode = Math.random().toString(36).replace(/[^a-z]+/g, '');
         filename = hashCode + '-' + file.originalname;
     }
 })
